ab37becfb17420231ab68d3c173c95143b1b6998
Dharani092 <88099329+Dharani092@users.noreply.github.com>
2021-07-29 12:05:49 +0530

    Update SunshineSyncUtils.java

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.sync;
 
 import android.content.Context;
@@ -43,11 +29,7 @@ public class SunshineSyncUtils {
 
     private static final String SUNSHINE_SYNC_TAG = "sunshine-sync";
 
-    /**
-     * Schedules a repeating sync of Sunshine's weather data using FirebaseJobDispatcher.
-     * @param context Context used to create the GooglePlayDriver that powers the
-     *                FirebaseJobDispatcher
-     */
+    
     static void scheduleFirebaseJobDispatcherSync(@NonNull final Context context) {
 
         Driver driver = new GooglePlayDriver(context);
