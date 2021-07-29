0113e032cfdee31e939ef4e3e62ddbe17605c366
Dharani092 <88099329+Dharani092@users.noreply.github.com>
2021-07-29 12:01:29 +0530

    Update WeatherDbHelper.java

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.sunshine.data;
 
 import android.content.Context;
@@ -21,9 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
 
-/**
- * Manages a local database for weather data.
- */
+
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
 
@@ -36,12 +20,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    /**
-     * Called when the database is created for the first time. This is where the creation of
-     * tables and the initial population of the tables should happen.
-     *
-     * @param sqLiteDatabase The database.
-     */
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
